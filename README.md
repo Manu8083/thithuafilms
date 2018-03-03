3baaa866ddfa391e04de5a9917c7db1e59015a61
Manuel Romero <manu8083el@gmail.com>
2018-03-02 21:33:11 -0500

    se creo el archivo gulpfile y se asignaron tareas basicas

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ Primeros pasos
 + Es fundamental tener node.js instalado de manera global.
 + Cuando se baje el repositorio se debe hacer el respectivo npm-install para instalar las dependencias de desarrollo.
 + Se esta usando gulp.js para minificar el codigo y comprimir las imagenes, leer primeros pasos con gulp (https://frontendlabs.io/1669--gulp-js-en-espanol-tutorial-basico-primeros-pasos-y-ejemplos )
++ Si no se hace un fork de este repositorio se debe hacer un git pull siempre al inicar el trabajo y un git push al terminar el trabajo.
++ Para ejecutar las tareas del gulp basica, minificacion de css,js y compresion de imagenes se debe teclear el comando "gulp dev"
+
+
+Aprendizaje adicional
+----------------------
+
++ Esta guia explica como instalar y usar los plugins mas importantes y utiles para gulp.js https://frontendlabs.io/1752--gulp-js-plugins-como-configurar-tareas
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,37 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	stylus = require('gulp-stylus'),
+	imagemin = require('gulp-imagemin');
 
-gulp.task('default', function(){
-	// ponga aqui la tarea mi pez;
+
+var pathCSS = {
+    stylus: ['stylus/*.styl'],
+    css: 'build/css/'
+};
+
+
+gulp.task('imagenes',function(){
+
+	return gulp.src(['src/imagenes/*.*'])
+        .pipe(imagemin())
+        .pipe(gulp.dest('build/images/'));
+
 });
+
+gulp.task('estilos',['imagenes'], function () {
+    return gulp.src(pathCSS.stylus)
+        .pipe(stylus({
+            compress: true
+        }))
+        .pipe(gulp.dest(pathCSS.css));
+});
+
+gulp.task('js', ['estilos'], function () {
+	gulp.src('js/*.js')
+	.pipe(concat('todo.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('build/js'))
+});
+
+gulp.task('dev', ['imagenes', 'estilos', 'js']);
